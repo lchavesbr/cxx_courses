47eea53f64304442c7931f1ca7ea2fb31f0733ac
Leonardo Chaves <lchavesbr@gmail.com>
2025-10-09 23:23:26 -0300

    First commit, uploading settings, vscode extensions installation scripts and task_1 of HW 1

---
+++ Uni-Bonn/README.md
@@ -0,0 +1,15 @@
+# Description
+
+The files you will find in the subdirectories are a free and indivitual attempt to follow the homeworks included in **Modern C++ for Computer Vision** (Lecture & Coding, MSc GuG & GE, summer) course (2020), as a modern cpp learning journey.
+
+Lecturers: T. Guadagnino,  Prof. C. Stachniss, I. Vizzo
+Tutor: I. Vizzo, T. Guadagnino
+
+Link to the course webpage:
+<https://www.ipb.uni-bonn.de/teaching/cpp-2020/index.html>
+
+All credits to StachnissLab, Photogrammetry & Robotics
+<https://www.ipb.uni-bonn.de/index.html>
+
+It is a great introduction to Modern C++ and give lots hints, examples, best practicies and references to all who want to deep dive in develping such wide range applications based on C++.
+Thanks and congratulations!
---
+++ Uni-Bonn/install_vscode_extensions.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# @file		install_vscode_extensions.sh
+# @author	Leonardo Chaves		[lchavesbr@gmail.com]
+#
+# Copyright (c) 2025 Leonardo Chaves, all rights reserved
+echo "Installing Visual Code Extensions for Modern C++ Course"
+
+# -------------------
+# VS Code Extensions
+# -------------------
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ms-vscode.cpptools
+code --install-extension jbenden.c-cpp-flylint
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension twxs.cmake
+code --install-extension ms-vscode.cmake-tools
+code --install-extension cheshirekow.cmake-format
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension TomasHubelbauer.vscode-markdown-table-format
+
+
+
