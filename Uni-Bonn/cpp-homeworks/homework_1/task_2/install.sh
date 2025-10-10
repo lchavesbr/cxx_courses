e67d8e7fdb2203e00b2de5feb3851a6107b5b481
Leonardo Chaves <lchavesbr@gmail.com>
2025-10-10 09:02:17 -0300

    Upload install.sh as part of task_2

---
+++ Uni-Bonn/cpp-homeworks/homework_1/task_2/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# @Description: Shell script to install results of task_2 of HW1
+# @Author: Leonardo Anjos Chaves        [lchavesbr@gmail.com]
+# @Date: 11/Oct/2025
+# @Modified:
+# Copyright (c) 2025 Leonardo Chaves, all rights reserved
+
+# copying to resultd subfolder
+cp build/task_2 results/bin
+cp build/libipb_arithmetic.a results/lib
