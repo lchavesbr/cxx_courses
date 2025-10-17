f6fef286245636f56b5080819dc116a31e5e735d
Leonardo Chaves <lchavesbr@gmail.com>
2025-10-17 07:33:13 -0300

    Project folder structure to hold all source libraries and included files.

---
+++ Uni-Bonn/cpp-homeworks/homework_3/homework_3/homework_3.h
@@ -0,0 +1,10 @@
+// @file      homework_3.h
+// @author    Leonardo Chaves     [lchavesbr@gmail.com]
+//
+// Copyright (c) 2025 Leonardo Chaves, all rights reserved
+#ifndef HOMEWORK_3_H_
+#define HOMEWORK_3_H_
+
+#include <homework_3/html_writer/html_writer.hpp>
+
+#endif  // HOMEWORK_3_H
---
+++ Uni-Bonn/cpp-homeworks/homework_3/homework_3/html_writer/html_writer.cpp
@@ -0,0 +1,27 @@
+// @file      html_writer.cpp
+// @author    Leonardo Chaves     [lchavesbr@gmail.com]
+//
+// Copyright (c) 2025 Leonardo Chaves, all rights reserved
+
+#include <homework_3/html_writer/html_writer.hpp>
+
+namespace html_writer {
+
+void OpenDocument() {}
+
+void CloseDocument() {}
+
+void AddCSSStyle(const std::string& stylesheet) {}
+
+void AddTitle(const std::string& title) {}
+
+void OpenBody() {}
+
+void CloseBody() {}
+
+void OpenRow() {}
+
+void CloseRow() {}
+
+void AddImage(const std::string& img_path, float score, bool highlight) {}
+}  // namespace html_writer
