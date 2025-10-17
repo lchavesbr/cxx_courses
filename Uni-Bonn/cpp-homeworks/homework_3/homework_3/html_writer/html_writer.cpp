bbb200c76f95bc1cbedbea06ecf163abbc3f4a84
Leonardo Chaves <lchavesbr@gmail.com>
2025-10-17 09:06:23 -0300

    Update html_writer.cpp with string-literals to easily read and print html code.

--- Uni-Bonn/cpp-homeworks/homework_3/homework_3/html_writer/html_writer.cpp
+++ Uni-Bonn/cpp-homeworks/homework_3/homework_3/html_writer/html_writer.cpp
@@ -3,25 +3,68 @@
 //
 // Copyright (c) 2025 Leonardo Chaves, all rights reserved
 
+#include <filesystem>
 #include <homework_3/html_writer/html_writer.hpp>
+#include <iostream>
 
 namespace html_writer {
 
-void OpenDocument() {}
+void OpenDocument() {
+    std::string const kOpenDoc =
+            "<!DOCTYPE html>\n"
+            "<html>\n";
+    std::cout << kOpenDoc;
+}
 
-void CloseDocument() {}
+void CloseDocument() {
+    std::string const kCloseDoc = "</html>";
+    std::cout << kCloseDoc;
+}
 
-void AddCSSStyle(const std::string& stylesheet) {}
+void AddCSSStyle(const std::string& stylesheet) {
+    std::string const kCSSStyle =
+            R"(<link rel="stylesheet" type="text/css" href=")" + stylesheet +
+            R"(" </>)" + "\n";
+    std::cout << kCSSStyle;
+}
 
-void AddTitle(const std::string& title) {}
+void AddTitle(const std::string& title) {
+    std::cout << R"(<title>)" << title << R"(</title>)" << "\n";
+}
 
-void OpenBody() {}
+void OpenBody() { std::cout << "<body>\n"; }
 
-void CloseBody() {}
+void CloseBody() { std::cout << "</body>\n"; }
 
-void OpenRow() {}
+void OpenRow() { std::cout << R"(<div class="row">)" << "\n"; }
 
-void CloseRow() {}
+void CloseRow() { std::cout << "</div>\n"; }
 
-void AddImage(const std::string& img_path, float score, bool highlight) {}
+void AddImage(const std::string& img_path, float score, bool highlight) {
+    std::string const kExtension_png{".png"};
+    std::string const kExtension_jpg{".jpg"};
+    std::filesystem::path const html_img_path{img_path};
+    std::string const img_title{html_img_path.stem()};
+    std::string const img_extension{html_img_path.extension()};
+
+    if (img_extension != kExtension_jpg && img_extension != kExtension_png) {
+        std::cerr << "File extension not supported:\t" << img_extension << "\n";
+        std::cerr << "Supported file formats are: \t" << kExtension_jpg
+                  << " and " << kExtension_png << "\n";
+        return;
+    }
+
+    std::string column = R"(<div class="column")";
+
+    if (highlight) {
+        column += R"( style="border: 5px solid green;")";
+    }
+    column += ">\n";
+    std::cout << column;
+    std::cout << "<h2>" << img_title + img_extension << "</h2>\n";
+    std::cout << "<img src =\"" << img_path << "\" />" << std::endl;
+    std::cout << "<p>score = " << std::setprecision(2) << std::fixed << score
+              << "</p>\n";
+    std::cout << "</div>\n";
+}
 }  // namespace html_writer
