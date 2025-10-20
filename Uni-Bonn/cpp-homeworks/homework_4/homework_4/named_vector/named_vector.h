d3016106c31d80acf23531b1518406f0292f3f1b
Leonardo Chaves <lchavesbr@gmail.com>
2025-10-19 23:44:37 -0300

    Update constructors definition without <> to compy with modernize clang-tidy specs

--- Uni-Bonn/cpp-homeworks/homework_4/homework_4/named_vector/named_vector.h
+++ Uni-Bonn/cpp-homeworks/homework_4/homework_4/named_vector/named_vector.h
@@ -15,32 +15,28 @@ namespace ipb {
 
 template <typename T>
 struct named_vector {
-    std::string name_;
-    std::vector<T> std_vector_;
+  std::string name_;
+  std::vector<T> std_vector_;
 
-    named_vector<T>() : name_{}, std_vector_{} {};
+  named_vector() : name_{}, std_vector_{} {};
 
-    named_vector<T>(const std::string& name, const std::vector<T>& vec)
-        : name_{name}, std_vector_{vec} {};
+  named_vector(const std::string& name, const std::vector<T>& vec)
+      : name_{name}, std_vector_{vec} {};
 
-    std::vector<T>& vector() { return std_vector_; };
-    [[nodiscard]] std::string& name() { return name_; };
+  std::vector<T>& vector() { return std_vector_; };
+  [[nodiscard]] std::string& name() { return name_; };
 
-    [[nodiscard]] int size() const {
-        return (name_.size() + std_vector_.size());
-    };
+  [[nodiscard]] int size() const {
+    return (name_.size() + std_vector_.size());
+  };
 
-    [[nodiscard]] bool empty() const {
-        return (name_.empty() || std_vector_.empty());
-    };
+  [[nodiscard]] bool empty() const {
+    return (name_.empty() || std_vector_.empty());
+  };
 
-    void reserve(std::size_t new_capacity) {
-        std_vector_.reserve(new_capacity);
-    };
-    void resize(std::size_t new_size) { std_vector_.resize(new_size); };
-    [[nodiscard]] std::size_t capacity() const {
-        return std_vector_.capacity();
-    };
+  void reserve(std::size_t new_capacity) { std_vector_.reserve(new_capacity); };
+  void resize(std::size_t new_size) { std_vector_.resize(new_size); };
+  [[nodiscard]] std::size_t capacity() const { return std_vector_.capacity(); };
 };
 
 };  // namespace ipb
